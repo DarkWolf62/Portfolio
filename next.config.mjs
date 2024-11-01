6b530fffb7d1a4b9341675b2be37a06981e32a02
nicolas <nicolasnourry61@gmail.com>
2024-11-01 15:44:59 +0100

    encore test image4

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/portfolio',
-    assetPrefix: '/portfolio/',
-    distDir: 'out',
+    output: "export",
+    images: { unoptimized: true },
+
 };
 
 export default nextConfig;
